c86bb68b81f767eee4599a18a355cef150076e36
alvaroooom <munoz.fernandez.alvaro@iescamas.es>
2026-01-19 10:56:03 +0100

    Entidad Producto creada

---
+++ proyecto/src/main/java/model/Producto.java
@@ -0,0 +1,66 @@
+package model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Entity
+public class Producto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Size(min = 2, max = 100)
+    private String nombre;
+
+    @NotBlank
+    @Size(max = 255)
+    private String descripcion;
+
+    @NotNull
+    private Double precio;
+
+    public Producto() {
+    }
+
+    public Producto(String nombre, String descripcion, Double precio) {
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.precio = precio;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(Double precio) {
+        this.precio = precio;
+    }
+}
